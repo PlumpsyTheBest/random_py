19ba2a4fca2580cafe3d0c561ffff70b66c398f8
PlumpsyTheBest <plumpsy@protonmail.com>
2024-10-30 14:36:04 +0000

    Update Lesson1Task6.py

--- Lesson1Task6.py
+++ Lesson1Task6.py
@@ -4,16 +4,16 @@ from random import randint
 # Generate an array for 6 random numbers between 1 and 60
 def generate_random_numbers():
     random_number_array = [0] * 6
-    for i in range(0, len(random_number_array)):
-        random_number_array[i] = randint(1, 60)
+    for counter in range(len(random_number_array)):
+        random_number_array[counter] = randint(1, 60)
     return random_number_array
 
 
 # Display any number from the array that is over 20
 def display_over_20(numbers):
-    for i in range(0, len(numbers)):
-        if numbers[i] > 20:
-            print(numbers[i])
+    for counter in range(len(numbers)):
+        if numbers[counter] > 20:
+            print(numbers[counter])
 
 
 # Main program
